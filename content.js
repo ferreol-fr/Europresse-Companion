fed0d92e2aecb529751bf47ab6a61f966b5dbafd
ferreol-fr <ponsich@laposte.net>
2025-07-04 11:01:17 +0200

    Differentiation cu css pour android. Ajout d'images pour les boutons enregistrement de la taille du menu sauf pour android

--- content.js
+++ content.js
@@ -1,8 +1,11 @@
 function createMenu() {
+    const path = window.location.pathname;
+    if (path.startsWith("Login")) return;
 
     let isEdited = false;
+
     // Créer le conteneur du menu
-    const menuContainer = document.createElement('span');
+    const menuContainer = document.createElement('div');
     menuContainer.id = 'my-ext-dropdown-menu-container';
     menuContainer.innerHTML = `
     <div class="my-ext-dropdown-menu">
@@ -49,29 +52,31 @@ function createMenu() {
             const menuItem = document.createElement('li');
             menuItem.className = 'my-ext-menu-item';
             menuItem.innerHTML = `
-        <button class="my-ext-content-button">${item.text}</button>
-        <button class="my-ext-edit-button" title="Éditer">
+        <table class="my-ext-item-table"><tr><td rowspan="2">
+        <button class="my-ext-content-button">${item.text}</button></td>
+        <td class="my-ext-small-td"><button class="my-ext-edit-button" title="Éditer">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
           <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
         </svg>
-      </button>
-      <button class="my-ext-delete-button" title="Supprimer">
+      </button></td>
+      <td class="my-ext-small-td"><button class="my-ext-move-up-button" title="Monter">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+        </svg>
+      </button></td></tr>
+      <td class="my-ext-small-td"><button class="my-ext-delete-button" title="Supprimer">
         <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
           <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
         </svg>
-      </button>
-      <button class="my-ext-move-up-button" title="Monter">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-        </svg>
-      </button>
-      <button class="my-ext-move-down-button" title="Descendre">
+      </button></td>      
+      <td class="my-ext-small-td"><button class="my-ext-move-down-button" title="Descendre">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
         </svg>
       </button>
+      </td></tr></table>
       `;
             menuList.insertBefore(menuItem, noResultsMessage);
         });
@@ -81,11 +86,35 @@ function createMenu() {
     }
 
     // Gérer l'affichage/masquage du menu
-    menuContainer.querySelector('.my-ext-dropdown-button').addEventListener('click', function() {
+    menuContainer.querySelector('.my-ext-dropdown-button').addEventListener('click', async function () {
+
+        if (!/Android/.test(navigator.userAgent)) {
+            await chrome.storage.local.get(['menuListSize'], function (result) {
+                if (result.menuListSize.width !== undefined) {
+                    menuList.style.width = result.menuListSize.width;
+                    menuList.style.height = result.menuListSize.height;
+                }
+            });
+        }
         menuList.classList.toggle('show');
         if (menuList.classList.contains('show')) {
             searchInput.focus();
         }
+
+    });
+
+    // Fermer le menu lorsqu'on clique à l'extérieur
+    document.addEventListener('click', async function (e) {
+        if (!menuContainer.contains(e.target) && !isEdited) {
+            if (!/Android/.test(navigator.userAgent) && menuList.style.width !== "") {
+                let menuListSize = {};
+                menuListSize.width = menuList.style.width;
+                menuListSize.height = menuList.style.height;
+                await chrome.storage.local.set({menuListSize: menuListSize});
+            }
+            menuList.classList.remove('show');
+        }
+        isEdited = false;
     });
 
     // Gérer la recherche
@@ -131,9 +160,9 @@ function createMenu() {
     }
 
     // Gérer les autres événements (délégation d'événements)
-    menuList.addEventListener('click', function(e) {
-        const target = e.target.closest('button') ;
-        let menuItem = false;
+    menuList.addEventListener('click', function (e) {
+        const target = e.target.closest('button');
+        let menuItem;
         try {
             menuItem = target.closest('.my-ext-menu-item');
         } catch (err) {
@@ -142,44 +171,42 @@ function createMenu() {
         if (!menuItem) return;
 
         if (target.classList.contains('my-ext-content-button')) {
-                const menuText = menuItem.querySelector('.my-ext-content-button');
-                console.log(menuText);
-                document.getElementById('Keywords').value = menuText.textContent;
-                document.getElementById('btnSearch').click();
+            const menuText = menuItem.querySelector('.my-ext-content-button');
+            console.log(menuText);
+            document.getElementById('Keywords').value = menuText.textContent;
+            document.getElementById('btnSearch').click();
         }
 
-        console.log('my-ext-edit-button',target);
+        console.log('my-ext-edit-button', target);
         if (target.classList.contains('my-ext-edit-button')) {
             const menuButon = menuItem.querySelector('.my-ext-content-button');
             const menuButonText = menuButon.textContent;
-            const menuText = document.createElement('li');
             menuItem.className = 'my-ext-menu-item';
             menuItem.innerHTML = `
-        <textarea class="my-ext-content-button">${menuButonText}</textarea>
-        <button class="my-ext-record-button" title="Valider les modifications">
+        <table class="my-ext-item-table"><tr><td rowspan="2">
+        <textarea class="my-ext-content-button">${menuButonText}</textarea></td>
+        <td class="my-ext-small-td"><button class="my-ext-record-button" title="Valider les modifications">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
         <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.477-4.425a.235.235 0 0 1 .02-.022z"/>
-        </svg></button>
-        <button class="my-ext-delete-button" title="Supprimer">
+        </svg></button></td>
+        <td class="my-ext-small-td"><button class="my-ext-move-up-button" title="Monter">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+        </svg>
+      </button></td></tr>
+        <td class="my-ext-small-td"><button class="my-ext-delete-button" title="Supprimer">
         <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
           <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
         </svg>
-      </button>
-      <button class="my-ext-move-up-button" title="Monter">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-        </svg>
-      </button>
-      <button class="my-ext-move-down-button" title="Descendre">
+      </button></td>  
+      <td class="my-ext-small-td"><button class="my-ext-move-down-button" title="Descendre">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
         </svg>
-      </button>
+      </button></td></tr></table>
       `;
-        isEdited = true;
-           // menuItem.replaceChild(menuButon,menuText);
-            //menuButon.replaceWith(menuText);
+            isEdited = true;
 
         }
 
@@ -187,37 +214,36 @@ function createMenu() {
         if (target.classList.contains('my-ext-record-button')) {
             const menuButon = menuItem.querySelector('.my-ext-content-button');
             const newText = menuButon.value;
-            const menuText = document.createElement('li');
             menuItem.className = 'my-ext-menu-item';
             menuItem.innerHTML = `
-        <button class="my-ext-content-button">${newText}</button>
-        <button class="my-ext-edit-button" title="Éditer">
+        <table class="my-ext-item-table"><tr><td rowspan="2">
+        <button class="my-ext-content-button">${newText}</button></td>
+        <td class="my-ext-small-td"><button class="my-ext-edit-button" title="Éditer">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-        <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.477-4.425a.235.235 0 0 1 .02-.022z"/>
+          <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+          <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
         </svg>
-        </button>
-        <button class="my-ext-delete-button" title="Supprimer">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-        </svg>
-      </button>
-      <button class="my-ext-move-up-button" title="Monter">
+      </button></td>
+      <td class="my-ext-small-td"><button class="my-ext-move-up-button" title="Monter">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
         </svg>
-      </button>
-      <button class="my-ext-move-down-button" title="Descendre">
+      </button></td></tr>
+      <td class="my-ext-small-td"><button class="my-ext-delete-button" title="Supprimer">
+        <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+        </svg>
+      </button></td>      
+      <td class="my-ext-small-td"><button class="my-ext-move-down-button" title="Descendre">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
         </svg>
       </button>
+      </td></tr></table>
       `;
 
-            //menuItem.replaceChild(menuButon,menuText);
-            //menuButon.replaceWith(menuText);
-            //menuContainer.refresh();
-            chrome.storage.local.get(['menuItems'], function(result) {
+            chrome.storage.local.get(['menuItems'], function (result) {
                 const menuItems = result.menuItems || [];
                 const allItems = Array.from(menuList.querySelectorAll('.my-ext-menu-item'));
                 const index = allItems.indexOf(menuItem);
@@ -230,7 +256,7 @@ function createMenu() {
         }
 
         if (target.classList.contains('my-ext-delete-button')) {
-            chrome.storage.local.get(['menuItems'], function(result) {
+            chrome.storage.local.get(['menuItems'], function (result) {
                 const menuItems = result.menuItems || [];
                 const allItems = Array.from(menuList.querySelectorAll('.my-ext-menu-item'));
                 const index = allItems.indexOf(menuItem);
@@ -250,7 +276,7 @@ function createMenu() {
                 menuList.insertBefore(menuItem, prevItem);
 
                 // Mettre à jour le stockage
-                chrome.storage.local.get(['menuItems'], function(result) {
+                chrome.storage.local.get(['menuItems'], function (result) {
                     const menuItems = result.menuItems || [];
                     // Échanger les éléments dans le tableau
                     [menuItems[index], menuItems[index - 1]] = [menuItems[index - 1], menuItems[index]];
@@ -268,7 +294,7 @@ function createMenu() {
                 menuList.insertBefore(nextItem, menuItem);
 
                 // Mettre à jour le stockage
-                chrome.storage.local.get(['menuItems'], function(result) {
+                chrome.storage.local.get(['menuItems'], function (result) {
                     const menuItems = result.menuItems || [];
                     // Échanger les éléments dans le tableau
                     [menuItems[index], menuItems[index + 1]] = [menuItems[index + 1], menuItems[index]];
@@ -278,13 +304,21 @@ function createMenu() {
         }
     });
 
-    // Fermer le menu lorsqu'on clique à l'extérieur
-    document.addEventListener('click', function(e) {
-        if (!menuContainer.contains(e.target) && !isEdited) {
-            menuList.classList.remove('show');
-        }
-        isEdited = false;
-    });
+
+
+    if (/Android/.test(navigator.userAgent)) {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = chrome.runtime.getURL('styles/android-styles.css');
+        document.getElementById('logoIcon').style.display = 'none';
+        document.querySelector('span[title="FACIL\'iti"]').style.display = 'none';
+        document.head.appendChild(link);
+    } else {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = chrome.runtime.getURL('styles/default-styles.css');
+        document.head.appendChild(link);
+    }
 }
 
 // Appeler la fonction pour créer le menu lorsque le DOM est chargé
@@ -293,17 +327,3 @@ if (document.readyState === 'loading') {
 } else {
     createMenu();
 }
-
-// Écouter les messages du popup
-chrome.runtime.onMessage.addListener((request) => {
-    if (request.action === "toggleMenu") {
-        const menuList = document.querySelector('.my-ext-menu-list');
-        if (menuList) {
-            menuList.classList.toggle('show');
-            const searchInput = document.querySelector('.my-ext-menu-search');
-            if (menuList.classList.contains('show') && searchInput) {
-                searchInput.focus();
-            }
-        }
-    }
-});
