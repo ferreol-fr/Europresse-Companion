9622620c80bf1a0a4acd16ec398810ceebead4b1
ferreol-fr <ponsich@laposte.net>
2025-07-07 22:41:08 +0200

    Eviler que les positions puissent être manipulées pendant l'édition. Ajustement css

--- content.js
+++ content.js
@@ -390,6 +390,16 @@ function createMenu() {
 function renderMenuItem(text, criteriaSet, dateRange,ddlSort, criteriaExp, isInEditedMode = false) {
     let searchValue;
     let editButton;
+    let upButton = `<button class="my-ext-move-up-button" title="Monter">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+        </svg>
+      </button>`;
+    let downButton = `<button class="my-ext-move-down-button" title="Descendre">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
+        </svg>
+      </button>`;
     if (criteriaExp === undefined) criteriaExp = "";
     if (isInEditedMode) {
         searchValue = `<input type="text" class="my-ext-content-button" value="${text}" />`;
@@ -397,6 +407,8 @@ function renderMenuItem(text, criteriaSet, dateRange,ddlSort, criteriaExp, isInE
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
         <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.477-4.425a.235.235 0 0 1 .02-.022z"/>
         </svg></button>`;
+        upButton = upButton.replace('my-ext-move-up-button', 'my-ext-move-up-button disabled-element');
+        downButton = downButton.replace('my-ext-move-down-button', 'my-ext-move-down-button disabled-element');
     } else {
         searchValue = `<button class="my-ext-content-button">${text}</button>`;
         editButton = `<button class="my-ext-edit-button" title="Éditer">
@@ -414,22 +426,14 @@ function renderMenuItem(text, criteriaSet, dateRange,ddlSort, criteriaExp, isInE
         ${criteriaSet}${dateRange}${ddlSort}${criteriaExp}
         </td>
         <td class="my-ext-small-td">${editButton}</td>
-      <td class="my-ext-small-td"><button class="my-ext-move-up-button" title="Monter">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
-        </svg>
-      </button></td></tr>
+      <td class="my-ext-small-td">${upButton}</td></tr>
       <td class="my-ext-small-td"><button class="my-ext-delete-button" title="Supprimer">
         <svg  xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
           <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
           <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
         </svg>
       </button></td>      
-      <td class="my-ext-small-td"><button class="my-ext-move-down-button" title="Descendre">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
-        </svg>
-      </button>
+      <td class="my-ext-small-td">${downButton}
       </td></tr></table>
       `;
     return menuItem;
